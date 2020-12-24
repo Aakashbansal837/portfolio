a055e301973b17c1043ca323db0a178c1c7484a3
aakashbansal837 <aakashbansal837@gmail.com>
2020-12-24 15:55:41 +0530

    + header

---
+++ src/components/Home/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div class="page-title home text-center">
+        <h2>
+          Aakash <span class="primary">Bansal</span>{" "}
+          <span class="title-bg">David</span>
+        </h2>
+        <p>
+          A product designer from England, who focuses on interactive design
+          &amp; A freelance designer focusing on typography &amp; clean
+          interfaces. Also works in Google.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
