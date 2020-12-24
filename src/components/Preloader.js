b5cb57d9a853c95bf20b87d3f77cbc55a1a221ed
aakashbansal837 <aakashbansal837@gmail.com>
2020-12-24 16:44:47 +0530

    + preloader

---
+++ src/components/Preloader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div class="preloader">
+      <div class="loading-mask"></div>
+      <div class="loading-mask"></div>
+      <div class="loading-mask"></div>
+      <div class="loading-mask"></div>
+      <div class="loading-mask"></div>
+    </div>
+  );
+};
+
+export default Preloader;
