42b1924aa958f2b1a4212b9a5ef63272a19dbbfd
aakashbansal837 <aakashbansal837@gmail.com>
2020-12-24 15:55:30 +0530

    + hexagon tiles

---
+++ src/components/Home/Hexagon.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Hexagon = ({ title }) => {
+  return (
+    <>
+      <div class="hexagon-item">
+        <div class="hex-item">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="hex-item">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <a href="welcome.html" class="hex-content">
+          <span class="hex-content-inner">
+            <span class="icon">
+              <i class="tf-dial"></i>
+            </span>
+            <span class="title">{title ? title : "none"}</span>
+          </span>
+          <svg
+            viewBox="0 0 173.20508075688772 200"
+            height="200"
+            width="174"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+              fill="#1e2530"
+            ></path>
+          </svg>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Hexagon;
