073a9f94edf9b08266cea4dcf3121111b789d48b
aakashbansal837 <aakashbansal837@gmail.com>
2020-12-24 15:54:55 +0530

    + hexagon menu

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import Header from "./Header";
+import Hexagon from "./Hexagon";
 const Home = () => {
-  return <div className="">Home</div>;
+  return (
+    <>
+      <Header />
+      <div class="hexagon-menu clear">
+        <Hexagon title="home" />
+        <Hexagon title="About" />
+        <Hexagon title="services" />
+        <Hexagon title="resume" />
+        <Hexagon title="works" />
+        <Hexagon title="testimonial" />
+        <Hexagon title="contact" />
+      </div>
+    </>
+  );
 };
 
 export default Home;
