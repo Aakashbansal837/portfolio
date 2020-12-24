1bc7a7825bec2bf6835b8851fec586609c823b0f
aakashbansal837 <aakashbansal837@gmail.com>
2020-12-24 19:58:50 +0530

    + icons , links

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,16 +5,16 @@ import Hexagon from "./Hexagon";
 const Home = () => {
   return (
     <>
-      <div className="col-xs-12 col-md-10 col-lg-8 center">
+      <div className="col-xs-12 col-sm-12 col-md-10 col-lg-8 center">
         <Header />
         <div className="hexagon-menu clear">
-          <Hexagon title="home" />
-          <Hexagon title="About" />
-          <Hexagon title="services" />
-          <Hexagon title="resume" />
-          <Hexagon title="works" />
-          <Hexagon title="testimonial" />
-          <Hexagon title="contact" />
+          <Hexagon title="home" icon="tf-dial" url="/" />
+          <Hexagon title="About" icon="tf-profile-male" url="/" />
+          <Hexagon title="services" icon="tf-tools-2" url="/" />
+          <Hexagon title="resume" icon="tf-tools" url="/" />
+          <Hexagon title="works" icon="tf-briefcase2" url="/" />
+          <Hexagon title="testimonial" icon="tf-chat" url="/" />
+          <Hexagon title="contact" icon="tf-envelope2" url="/" />
         </div>
       </div>
     </>
