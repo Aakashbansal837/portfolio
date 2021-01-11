f367b435cad8681c7c0c5c2e55185f6e939e4aa9
aakashbansal837 <aakashbansal837@gmail.com>
2021-01-11 21:29:59 +0530

    ^ constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,17 @@
+// import * as blueColor from "../style/colors/blue.css";
+// import * as redColor from "../style/colors/red.css";
+// import * as yellowcolor from "../style/colors/yellow.css";
+// import * as greenColor from "../style/colors/green.css";
+// const themeColors = {
+//   blue: blueColor,
+//   red: redColor,
+//   yellow: yellowcolor,
+//   green: greenColor,
+// };
 const constants = {
   mode: "dark", // this will alter between day (white)  and night(black)
   themeColor: "blue", // can have variour options
+  // themeColors: themeColors,
 };
 
 export default constants;
