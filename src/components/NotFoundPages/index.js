6c73457f9f97e346c4554e0bb3d69ee4c88b7738
aakashbansal837 <aakashbansal837@gmail.com>
2021-01-12 11:50:12 +0530

    + not found page

---
+++ src/components/NotFoundPages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPages = () => {
+  return (
+    <h1 style={{ fontWeight: 800, color: "red", opacity: 1 }}>
+      {" "}
+      404 data not found
+    </h1>
+  );
+};
+
+export default NotFoundPages;
